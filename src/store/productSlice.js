f2b583cec2b26b71426a672de596a42391e207f4
rashi-06 <rashisharma18062001@gmail.com>
2023-07-27 22:00:44 +0530

    created a productSlice file

---
+++ src/store/productSlice.js
@@ -0,0 +1,27 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+const initialState = {
+    data : [],
+};
+
+const productSlice = createSlice({
+    name:"product",
+    initialState,
+    reducers : {
+        add(state , action){
+            state.push(action.payload);
+        },
+        remove(state , action){
+            return state.filter(item => item.id !== action.payload);
+        }
+    }
+})
+
+
+export const getProducts = () =>{
+     
+}
+
+
+export const {add , remove} = productSlice.actions
+export default productSlice.reducer
